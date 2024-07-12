7732be7c340b30d59994dd8ef1714c2d034b7840
Thorin215 <1216513790@qq.com>
2024-07-13 00:34:41 +0800

    [背景区分障碍和背景]

--- Common/Background.h
+++ Common/Background.h
@@ -1,7 +1,3 @@
-//
-// Created by 16579 on 2024/7/9.
-//
-
 #ifndef DAD_N_ME_BACKGROUND_H
 #define DAD_N_ME_BACKGROUND_H
 
@@ -9,7 +5,7 @@
 #include <QPainter>
 #include <QImage>
 #include <QRectF>
-#include <QRandomGenerator>
+#include <QVector>
 #include "Common.h"
 #include "ResourceManager.h"
 #include "Animation.h"
@@ -18,71 +14,79 @@ class Background {
 public:
     QColor backgroundColor; // 背景颜色
     QVector<QImage> obstacleImages;   // 障碍物图片
-    bool obstaclePlaced;    // 是否已经放置障碍物
-    std::vector<Point> positions;
-
-
-    QRectF healthBarRect;   //血条
-    QRectF patienceBarRect;   //精力条
-
+    std::vector<QPoint> obPositions;    // 障碍物位置
+    QVector<QImage> backgroundImages;
+    std::vector<QPoint> bgPositions;    // 障碍物位置
+    QRectF healthBarRect;   // 血条
+    QRectF patienceBarRect; // 精力条
     float health = 1.0;
     float patience = 1.0;
 
 public:
-    Background(const QColor& color = Qt::white) : backgroundColor(color), obstaclePlaced(true){
-        obstacleImages.append(ResourceManager::getInstance().getImage("../resource/background/house_brown.png"));
-        positions.push_back({50, 50});
-        obstacleImages.append(ResourceManager::getInstance().getImage("../resource/background/bush1.png"));
-        positions.push_back({400,400});
-
-
+    Background(const QColor& color = Qt::white) : backgroundColor(color) {
+        initializeObstacles(); // 初始化障碍物
+        initializeBackgrounds();
         healthBarRect = QRectF(25, 25, 200, 20);
         patienceBarRect = QRectF(25, 60, 200, 20);
     }
-
 
     void draw(QPainter* painter, const QRectF& rect, bool showHealthBar) {
         // 绘制背景颜色
         painter->fillRect(rect, backgroundColor);
 
-        // 绘制背景颜色
-        painter->fillRect(rect, backgroundColor);
 
 
-        for(int i = 0; i < 2 ; i++) {
-            painter->drawImage(positions[i].x, positions[i].y, obstacleImages[i]);
+        for (const auto& pos : bgPositions) {
+            int index = &pos - &bgPositions[0]; // 计算当前位置的索引
+            painter->drawImage(pos, backgroundImages[index]);
+        }
+
+        // 绘制障碍物
+        for (const auto& pos : obPositions) {
+            int index = &pos - &obPositions[0]; // 计算当前位置的索引
+            painter->drawImage(pos, obstacleImages[index]);
         }
 
         // 绘制血条和精力条
         if (showHealthBar) {
-            QPen borderPen(Qt::black); // 设置边框颜色为黑色
-            borderPen.setWidth(3);     // 设置边框宽度为3个像素，可以根据需要调整粗细
-            borderPen.setStyle(Qt::SolidLine); // 设置边框线条样式为实线
-            painter->setPen(borderPen);
-
-            // 绘制血条边框
-            painter->drawRect(healthBarRect);
-
-            // 绘制血条内部（红色部分）
-            QRectF innerRect = healthBarRect.adjusted(3, 3, -3, -3); // 调整内部矩形，留出边框宽度的空间
-            QRectF healthRect = QRectF(innerRect.x(), innerRect.y(), innerRect.width() * health, innerRect.height());
-            painter->fillRect(healthRect, Qt::red);
-
-            painter->drawRect(patienceBarRect);
-            innerRect = patienceBarRect.adjusted(3, 3, -3, -3); //调整内部矩形，留出边框宽度的空间
-            QRectF patienceRect = QRectF(innerRect.x(), innerRect.y(), innerRect.width() * patience, innerRect.height());
-            painter->fillRect(patienceRect, Qt::green);
-
+            drawProgressBar(painter, healthBarRect, health, Qt::red);
+            drawProgressBar(painter, patienceBarRect, patience, Qt::green);
         }
     }
 
+    void addObstacles(QVector<QRect>* obstacles) {
+        for (const auto& pos : obPositions) {
+            int index = &pos - &obPositions[0]; // 计算当前位置的索引
+            obstacles->append(QRect(pos, obstacleImages[index].size()));
+        }
+    }
 
+private:
+    void initializeObstacles() {
+        // 添加障碍物图片和位置
+        obstacleImages.append(ResourceManager::getInstance().getImage("../resource/background/house_brown.png"));
+        obPositions.push_back(QPoint(50, 50));
+    }
 
+    void drawProgressBar(QPainter* painter, const QRectF& rect, float value, const QColor& color) {
+        QPen borderPen(Qt::black);
+        borderPen.setWidth(3);
+        borderPen.setStyle(Qt::SolidLine);
+        painter->setPen(borderPen);
+        painter->drawRect(rect);
 
-    void addObstacles(QVector<QRect>* obstacles){
-        obstacles->append(QRect(50, 50, obstacleImages[0].width(), obstacleImages[0].height()));
-        obstacles->append(QRect(400, 400, obstacleImages[1].width(), obstacleImages[1].height()));
+        QRectF innerRect = rect.adjusted(3, 3, -3, -3);
+        QRectF barRect = QRectF(innerRect.x(), innerRect.y(), innerRect.width() * value, innerRect.height());
+        painter->fillRect(barRect, color);
+    }
+
+    void initializeBackgrounds() {
+        backgroundImages.append(ResourceManager::getInstance().getImage("../resource/background/bush1.png"));
+        bgPositions.push_back(QPoint(400, 400));
+
+        backgroundImages.append(ResourceManager::getInstance().getImage("../resource/background/grand.png"));
+        bgPositions.push_back(QPoint(600, 200));
     }
 };
 
-#endif //DAD_N_ME_BACKGROUND_H
+#endif // DAD_N_ME_BACKGROUND_H
--- main.cpp
+++ main.cpp
@@ -1,27 +1,50 @@
-
 #include <QApplication>
+#include <QScreen>
+#include <QDebug>
 #include "View/GameWidget.h"
 #include "ViewModel/ViewModel.h"
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
+
+    // Initialize player signal
     int playerSignal = 0;
+
+    // Initialize view
     View_draw view;
-    /*ViewModel*/
-    GameViewModel viewModel = GameViewModel(nullptr);
-    /*View*/
-    GameWidget gameWidget(nullptr,viewModel.getBackground(), viewModel.getSmallEnemies(), viewModel.getBoss(),
-                          viewModel.getObstacles(), viewModel.getPlayer(),  &playerSignal, &view);
-    gameWidget.resize(1280, 720);
+
+    // Initialize ViewModel
+    GameViewModel viewModel(nullptr);
+
+    // Get primary screen geometry
+    QScreen *screen = QGuiApplication::primaryScreen();
+    QRect screenGeometry = screen->geometry();
+
+    // Initialize and set up the GameWidget
+    GameWidget gameWidget(nullptr,
+                          viewModel.getBackground(),
+                          viewModel.getSmallEnemies(),
+                          viewModel.getBoss(),
+                          viewModel.getObstacles(),
+                          viewModel.getPlayer(),
+                          &playerSignal,
+                          &view);
+
+    // Set GameWidget size to screen size
+    gameWidget.resize(screenGeometry.size());
+
+    // Show the GameWidget
     gameWidget.show();
 
+    // Connect key signals to the corresponding slots in the ViewModel
     QObject::connect(&gameWidget, &GameWidget::KeyLeft, &viewModel, &GameViewModel::handleKeyLeft);
     QObject::connect(&gameWidget, &GameWidget::KeyRight, &viewModel, &GameViewModel::handleKeyRight);
     QObject::connect(&gameWidget, &GameWidget::KeyUp, &viewModel, &GameViewModel::handleKeyUp);
     QObject::connect(&gameWidget, &GameWidget::KeyDown, &viewModel, &GameViewModel::handleKeyDown);
     QObject::connect(&gameWidget, &GameWidget::KeyS, &viewModel, &GameViewModel::handleKeyS);
+
+    // Start the Qt event loop
     return app.exec();
 }
 
-
-#include "main.moc"
+#include "main.moc"  // Include the MOC-generated implementation for signal/slot connections
